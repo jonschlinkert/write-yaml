b5aebe5dcd9808d6fe2c7a7f64d162bc6dc4de5d
jonschlinkert <dev@sellside.com>
2014-11-06 00:51:52 -0500

    v0.1.2
      - update deps, docs

--- index.js
+++ index.js
@@ -8,34 +8,13 @@
 
 var YAML = require('js-yaml');
 var writeFile = require('write');
-var extend = require('xtend');
+var extend = require('extend-shallow');
 
 var defaults = {
   indent:  2,
   skipInvalid:  false,
   flowLevel: -1
 };
-
-
-/**
- * **Example:**
- *
- * ```js
- * var yaml = require('write-yaml');
- * yaml('config.yml', {foo: {bar: {baz: 'quux'}}});
- * ```
- * Results in `config.yml` with the following contents:
- *
- * ```yaml
- * foo:
- *   bar:
- *     baz: quux
- * ```
- *
- * @param   {String} `dest` The destination filepath
- * @param   {String} `data` Object of data.
- * @param   {String} `options` Options to pass to js-yaml
- */
 
 module.exports = function (dest, data, options) {
   options = extend({}, defaults, options);
