88256de8d7f82d13e77b8d6f41360169043180ac
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-07 06:00:53 -0400

    refactor

--- index.js
+++ index.js
@@ -17,14 +17,43 @@ var defaults = {
   flowLevel: -1
 };
 
-module.exports = function (dest, data, options) {
-  options = extend({}, defaults, options);
-  var yaml = options.safe ? YAML.safeDump : YAML.dump;
+module.exports = function (dest, data, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts;
+    opts = {};
+  }
+
+  if (typeof cb !== 'function') {
+    throw new TypeError('write-yaml async expects a callback function.');
+  }
+  if (typeof dest !== 'string') {
+    return cb(new TypeError('write-yaml async expects dest to be a string.'));
+  }
+  if (typeof data !== 'object') {
+    return cb(new TypeError('write-yaml async expects data to be an object.'));
+  }
+
+  opts = extend({}, defaults, opts);
+  var yaml = opts.safe ? YAML.safeDump : YAML.dump;
+
+  writeFile(dest, yaml(data, opts), cb);
+};
+
+module.exports.sync = function(dest, data, opts) {
+  if (typeof dest !== 'string') {
+    throw new TypeError('write-yaml expects dest to be a string.');
+  }
+  if (typeof data !== 'object') {
+    throw new TypeError('write-yaml expects data to be an object.');
+  }
+
+  opts = extend({}, defaults, opts);
+  var yaml = opts.safe ? YAML.safeDump : YAML.dump;
 
   try {
-    return writeFile.sync(dest, yaml(data, options));
+    return writeFile.sync(dest, yaml(data, opts));
   } catch (err) {
-    err.message = 'Failed to write "' + dest + '": ' + err.message;
+    err.message = 'write-yaml: failed to write "' + dest + '": ' + err.message;
     throw err;
   }
 };
--- test/test.js
+++ test/test.js
@@ -7,25 +7,59 @@
 
 'use strict';
 
+/* deps:mocha */
 var fs = require('fs');
 var path = require('path');
 var should = require('should');
 var readYaml = require('read-yaml');
 var writeYaml = require('..');
+var should = require('should');
 
-describe('write yaml', function () {
+var files = ['tmp/a.md', 'tmp/b.md', 'tmp/c.md', 'tmp/d.md', 'tmp/e.md'];
+
+describe('sync', function () {
   it('should write a yaml file synchronously', function () {
-    writeYaml('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}});
+    writeYaml.sync('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}});
     var json = readYaml.sync('test/actual/a.yml');
     json.a.should.have.property('b');
     json.a.b.should.have.property('c');
   });
 
   it('should use specified indentation.', function () {
-    writeYaml('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4});
+    writeYaml.sync('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4});
     var str = fs.readFileSync('test/actual/b.yml', 'utf8');
     var secondLine = str.split('\n')[1];
     var indent = secondLine.match(/^(\s+)/)[0].split('').length;
     indent.should.equal(4);
   });
 });
+
+describe('async', function () {
+  it('should write a yaml file asynchronously', function (cb) {
+    writeYaml('test/actual/a.yml', {a: {b: {c: {d: 'e'}}}}, function (err) {
+      if (err) {
+        console.log(err);
+        return cb(err);
+      }
+      var json = readYaml.sync('test/actual/a.yml');
+      json.a.should.have.property('b');
+      json.a.b.should.have.property('c');
+      cb();
+    });
+  });
+
+  it('should use specified indentation.', function (cb) {
+    writeYaml('test/actual/b.yml', {a: {b: {c: {d: 'e'}}}}, {indent: 4}, function (err) {
+      if (err) {
+        console.log(err);
+        return cb(err);
+      }
+      var str = fs.readFileSync('test/actual/b.yml', 'utf8');
+      var secondLine = str.split('\n')[1];
+      var indent = secondLine.match(/^(\s+)/)[0].split('').length;
+      indent.should.equal(4);
+      cb();
+    });
+  });
+});
+
